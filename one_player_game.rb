f1b54c28b0cdebb678496d6d468ac01949786707
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-30 19:55:02 -0400

    Starting of turning one_player_game file into module instead of class

--- one_player_game.rb
+++ one_player_game.rb
@@ -1,20 +1,18 @@
 module OnePlayerGame
 
-  attr_reader :number_1, :number_2
-
   def generate_question
-    @number_1 = rand(1..20)
-    @number_2 = rand(1..20)
-    @correct_answer = number_1 + number_2
+    number_1 = rand(1..20)
+    number_2 = rand(1..20)
+    correct_answer = number_1 + number_2
   end
 
   def ask_question
     until @player_lives == 0
       generate_question
       puts "Okay #{@name}, you currently have #{@player_lives} lives left and have #{@player_score} points."
-      puts ("What is #{@number_1} + #{@number_2}?").colorize(:blue)
+      puts ("What is #{number_1} + #{number_2}?").colorize(:blue)
       answer = gets.chomp.to_i
-      if answer == @correct_answer
+      if answer == correct_answer
         @player_score += 1
         puts ("Great work! 1 point added!").colorize(:green)
       else
